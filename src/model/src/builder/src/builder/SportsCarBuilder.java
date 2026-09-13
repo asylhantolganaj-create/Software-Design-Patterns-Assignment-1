a772c7fdf180fa30ba150a5bc84c28a3e64d59df
asylhantolganaj-create <asylhantolganaj@gmail.com>
2026-09-13 15:37:52 +0500

    Add SportsCarBuilder class for building sports cars
    Implemented SportsCarBuilder for constructing sports cars with validation for seat count.

---
+++ src/model/src/builder/src/builder/SportsCarBuilder.java
@@ -0,0 +1,53 @@
+package builder;
+
+import model.Car;
+import model.CarType;
+import model.Transmission;
+
+// Concrete builder for constructing sports cars
+public class SportsCarBuilder implements CarBuilder {
+    private CarType carType = CarType.SPORTS_CAR;
+    private String engine;
+    private Transmission transmission;
+    private int seats;
+    private boolean hasGPS;
+
+    @Override
+    public CarBuilder setCarType(CarType type) {
+        this.carType = type;
+        return this; // Method chaining (Fluent API)
+    }
+
+    @Override
+    public CarBuilder setEngine(String engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    @Override
+    public CarBuilder setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    @Override
+    public CarBuilder setSeats(int seats) {
+        this.seats = seats;
+        return this;
+    }
+
+    @Override
+    public CarBuilder setGPS(boolean hasGPS) {
+        this.hasGPS = hasGPS;
+        return this;
+    }
+
+    @Override
+    public Car build() {
+        // Validation: A sports car should not have more than 2 seats
+        if (seats > 2) {
+            throw new IllegalArgumentException("Sports car cannot have more than 2 seats!");
+        }
+        return new Car(carType, engine, transmission, seats, hasGPS);
+    }
+}
